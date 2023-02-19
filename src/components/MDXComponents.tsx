6eb7d64515b1eab7ef24159d3526428e372774f5
juancamilosalazarrestrepo <juancamilosalazarrestrepo@gmail.com>
2023-02-18 19:47:47 -0500

    fix bug tipes a

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -4,24 +4,8 @@ import Link, { LinkProps } from "next/link";
 import { text } from "stream/consumers";
 import Image from "next/image"
  
-const CustomLink = (props: React.ComponentProps<"a"> & LinkProps) => {
-  const href = props.href;
-  const isInternalLink =
-    href && (href.startsWith("/") || href.startsWith("#"));
-
-  if (isInternalLink) {
-    return (
-      <Link href={href} >
-        {href}
-      </Link>
-    );
-  }
-
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
-};
 
 const MDXComponents = {
-  a:CustomLink,
   Image,
   Link
 };
